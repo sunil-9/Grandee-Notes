7b9c4a2702041be3d1502a62df7706e70f346ee7
sunil sapkota <me@sapkotasunil.com.np>
2021-03-31 15:41:39 +0545

    notification layout file added

--- app/src/main/java/com/dhanas/grandeenotes/Notifications.java
+++ app/src/main/java/com/dhanas/grandeenotes/Notifications.java
@@ -1,41 +1,25 @@
-package com.dhanas.grandeenotes.Activity;
+package com.dhanas.grandeenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.appcompat.widget.Toolbar;
-
-import com.dhanas.grandeenotes.R;
+import com.dhanas.grandeenotes.Activity.AboutUs;
 import com.dhanas.grandeenotes.Utility.PrefManager;
-import com.dhanas.grandeenotes.Webservice.BaseURL;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
-import com.squareup.picasso.Picasso;
 
-import static com.squareup.picasso.Picasso.Priority.HIGH;
-
-public class AboutUs extends AppCompatActivity {
-
-    TextView txt_about_us, txt_app_name, txt_company, txt_email, txt_website, txt_mobile;
-    TextView toolbar_title;
-    Toolbar toolbar;
-
+public class Notifications extends AppCompatActivity {
     LinearLayout ly_back;
     PrefManager prefManager;
-
-    ImageView iv_app_icon;
-
     RelativeLayout rl_adView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,23 +30,8 @@ public class AboutUs extends AppCompatActivity {
             setTheme(R.style.AppTheme);
             getSupportActionBar().hide();
         }
-        setContentView(R.layout.about_us);
-        PrefManager.forceRTLIfSupported(getWindow(), AboutUs.this);
-        prefManager = new PrefManager(AboutUs.this);
-        toolbar = findViewById(R.id.toolbar);
-        toolbar_title = findViewById(R.id.toolbar_title);
-        toolbar_title.setText(getResources().getString(R.string.about_us));
-
+        setContentView(R.layout.activity_notifications);
         rl_adView = findViewById(R.id.rl_adView);
-        txt_app_name = findViewById(R.id.txt_app_name);
-        txt_company = findViewById(R.id.txt_company);
-        txt_email = findViewById(R.id.txt_email);
-        txt_website = findViewById(R.id.txt_website);
-        txt_mobile = findViewById(R.id.txt_mobile);
-        txt_about_us = findViewById(R.id.txt_about_us);
-
-        iv_app_icon = findViewById(R.id.iv_app_icon);
-
         ly_back = findViewById(R.id.ly_back);
         ly_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,28 +39,14 @@ public class AboutUs extends AppCompatActivity {
                 finish();
             }
         });
-        txt_app_name.setText(prefManager.getValue("app_name"));
-        txt_company.setText(prefManager.getValue("Author"));
-        txt_email.setText(prefManager.getValue("host_email"));
-        txt_website.setText(prefManager.getValue("website"));
-        txt_mobile.setText(prefManager.getValue("contact"));
-        txt_about_us.setText(prefManager.getValue("app_desripation"));
-
-        Picasso.with(AboutUs.this).load(BaseURL.Image_URL + "" + prefManager.getValue("app_logo")).priority(HIGH).into(iv_app_icon);
-
-        if (prefManager.getValue("banner_ad").equalsIgnoreCase("yes")) {
-            Admob();
-            rl_adView.setVisibility(View.VISIBLE);
-        } else {
-            rl_adView.setVisibility(View.GONE);
-        }
+        PrefManager.forceRTLIfSupported(getWindow(), Notifications.this);
+        prefManager = new PrefManager(Notifications.this);
 
     }
-
     public void Admob() {
 
         try {
-            AdView mAdView = new AdView(AboutUs.this);
+            AdView mAdView = new AdView(Notifications.this);
             mAdView.setAdSize(AdSize.SMART_BANNER);
             mAdView.setAdUnitId(prefManager.getValue("banner_adid"));
             AdRequest adRequest = new AdRequest.Builder().build();
